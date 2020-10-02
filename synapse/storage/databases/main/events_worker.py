8cbb160491cd0f4e7691a5a82c1414f5180876ad
Richard van der Hoff <richard@matrix.org>
2020-10-02 14:44:25 +0100

    Reduce duplication in event ordering lookup methods
    `get_topological_token_for_event` and `get_event_ordering` were doing almost
    the same thing: the difference was only in the format of the result (`tuple` vs
    `RoomStreamOrdering`).
    
    This commit combines the two. `get_event_ordering` had a cache which may have
    been tuned so we stick with that name, but update it to return a
    `RoomStreamOrdering` for better type safety. We also put it in
    `StreamWorkerStore` rather than `EventsWorkerStore` since that's where all the
    other "ordering" methods are.
    
    So then we have to update all the places `get_topological_token_for_event` was
    called, to call `get_event_ordering` instead, and we have to update all the
    places `get_event_ordering` was called (just `is_event_after`) to handle
    `RoomStreamToken`s intstead of tuples.
    
    `is_event_after` was only called in once place and we may as well inline it.

--- synapse/storage/databases/main/events_worker.py
+++ synapse/storage/databases/main/events_worker.py
@@ -1243,27 +1243,6 @@ class EventsWorkerStore(SQLBaseStore):
 
         return rows, to_token, True
 
-    async def is_event_after(self, event_id1, event_id2):
-        """Returns True if event_id1 is after event_id2 in the stream
-        """
-        to_1, so_1 = await self.get_event_ordering(event_id1)
-        to_2, so_2 = await self.get_event_ordering(event_id2)
-        return (to_1, so_1) > (to_2, so_2)
-
-    @cached(max_entries=5000)
-    async def get_event_ordering(self, event_id):
-        res = await self.db_pool.simple_select_one(
-            table="events",
-            retcols=["topological_ordering", "stream_ordering"],
-            keyvalues={"event_id": event_id},
-            allow_none=True,
-        )
-
-        if not res:
-            raise SynapseError(404, "Could not find event %s" % (event_id,))
-
-        return (int(res["topological_ordering"]), int(res["stream_ordering"]))
-
     async def get_next_event_to_expire(self) -> Optional[Tuple[str, int]]:
         """Retrieve the entry with the lowest expiry timestamp in the event_expiry
         table, or None if there's no more event to expire.
